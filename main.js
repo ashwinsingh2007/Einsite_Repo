867ca1de15b1731105fb6af6dedde2a24e94605f
ashwinsingh2007 <legend.ashwini07@gmail.com>
2017-05-21 19:37:51 +0530

    10

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var sessionsexp = require('express-session');
 var bodyParser = require('body-parser');
 var Client = require('node-rest-client').Client;
 var db=require('./Models/CreateUsers');
 var app = express();
+app.use(cookieParser());
+app.use(sessionsexp({resave: true, saveUninitialized: true, secret: 'einsite', cookie: { maxAge: 60000 }}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+
 app.use('/static', express.static(path.join(__dirname, 'public')));
 var index=0;
 var _session=false;
@@ -34,7 +38,7 @@ catch(ex){
 //this is to render index page for login
 app.get('/', function(req, res) {
     try{
-        if(_session==false){
+        if(req.session.user==undefined){
             res.sendFile(path.join(__dirname, './views', 'index.html'));
         }
         else{
@@ -49,7 +53,20 @@ app.get('/', function(req, res) {
 //this page will login after successfull registration or sign in
 app.get('/stock.html', function(req, res) {
     try{
-        if(_session){
+        if(!!req.session.user){
+            res.sendFile(path.join(__dirname, './views', 'stock.html')); 
+        }
+        else{
+            res.sendFile(path.join(__dirname, './views', 'index.html')); 
+        }
+    }
+    catch(ex){
+        console.log(ex);
+    }
+})
+app.get('/stock', function(req, res) {
+    try{
+        if(!!req.session.user){
             res.sendFile(path.join(__dirname, './views', 'stock.html')); 
         }
         else{
@@ -65,7 +82,7 @@ app.get('/stock.html', function(req, res) {
 //this page will show analysis of stock price in database and api call.
 app.get('/flot.html', function(req, res) {
     try{
-        if(_session){
+        if(!!req.session.user){
             res.sendFile(path.join(__dirname, './views', 'flot.html'));
         }
         else{
@@ -80,7 +97,7 @@ app.get('/flot.html', function(req, res) {
 //this page will show tables of records
 app.get('/tables.html', function(req, res) {
     try{
-        if(_session){
+        if(req.session.user){
             res.sendFile(path.join(__dirname, './views', 'tables.html'),{data: req.body});
         }
         else{
@@ -94,8 +111,14 @@ app.get('/tables.html', function(req, res) {
 
 app.get('/logout.html', function(req, res) {
     try{
-        _session=false;
-        res.sendFile(path.join(__dirname, './views', 'index.html'));
+        req.session.destroy(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.sendFile(path.join(__dirname, './views', 'index.html'));
+        }
+        });
+        
     }
     catch(ex){
         console.log(ex);
@@ -104,11 +127,12 @@ app.get('/logout.html', function(req, res) {
 
 app.post('/stock', function(req, res) {
     try{
-        if(_session==false){
+        if(req.session.user==undefined){
             if(!!req.body.emailLogin){
                 db.authorizeUser(req.body,function(result){
                     if(result){
-                        _session=true;
+                        req.session.user=req.body.emailLogin;
+                        console.log(req.session);
                         res.sendFile(path.join(__dirname, './views', 'stock.html'));
                     }
                     else{
@@ -118,7 +142,7 @@ app.post('/stock', function(req, res) {
             }else{
                 db.CreateUsers(req.body,function(result){
                     if (result) {   
-                            _session=true;
+                            req.session.user=req.body.emailLogin;
                             res.sendFile(path.join(__dirname, './views', 'stock.html'));
                         } else {
                         res.send("Error occured !! May be credentials already used for 'email'");                
